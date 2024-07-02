96aea24be815fa3fab3cec8881e0a0f004d18dbd
hyounce <hilde.younce@gmail.com>
2024-07-02 12:46:17 -0400

    Edited setup parameters

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='booklover',
+    version='1.0.0',
+    url='https://github.com/hyounce/ds5100-booklover',
+    author='hyounce',
+    author_email='ksg8xy@virginia.edu',
+    description='Contains Boolover module and tests.',
+    packages=['booklover'],
+)
